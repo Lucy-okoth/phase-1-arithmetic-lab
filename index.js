32cbdb70535d38c7c68cc59ae65c499eb936d3c4
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-08-18 11:58:32 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
+
+
+let num3 = 44;
+let num4 = 5;
+let mod = num3 %num4;
+console.log(mod);
+
+let max = 20;
+console.log(Math.max(1, 2, 3, 4, 20));
+
+let random= 1;
+Math.floor(Math.random() * 10) + 0;
